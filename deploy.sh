e85d8d7ac34caecfd2e66a47afba14b3fc776878
Roman Cherednychenko <Roman.Cheredichenko@gmail.com>
2021-04-09 23:07:53 +0200

    new media and tools

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+ ng build --prod --base-href "https://glorund.github.io/fotostrecke/"
+ npx angular-cli-ghpages --dir=dist/fotostrecke
--- setup.sh
+++ setup.sh
@@ -2,17 +2,19 @@
 
 SCRIPT_PATH=$(dirname "$0")
 
+export PATH=$PATH:/d/Program\ Files/ImageMagick-7.0.11-Q16-HDRI
+
 # mogrify is available
 # low res version of image
 python $SCRIPT_PATH/tools/duplicate.py min
-/c/Program\ Files/ImageMagick-7.0.11-Q16/magick.exe mogrify -resize 640x $SCRIPT_PATH/src/assets/photos/**/*.min.jpeg &>/dev/null
-/c/Program\ Files/ImageMagick-7.0.11-Q16/magick.exe mogrify -resize 640x $SCRIPT_PATH/src/assets/photos/**/*.min.png &>/dev/null
-/c/Program\ Files/ImageMagick-7.0.11-Q16/magick.exe mogrify -resize 640x $SCRIPT_PATH/src/assets/photos/**/*.min.jpg &>/dev/null
+magick.exe mogrify -resize 640x $SCRIPT_PATH/src/assets/photos/**/*.min.jpeg # &>/dev/null
+magick.exe mogrify -resize 640x $SCRIPT_PATH/src/assets/photos/**/*.min.png # &>/dev/null
+magick.exe mogrify -resize 640x $SCRIPT_PATH/src/assets/photos/**/*.min.jpg # &>/dev/null
 
 # placeholder image for lazy loading
 python $SCRIPT_PATH/tools/duplicate.py placeholder
-/c/Program\ Files/ImageMagick-7.0.11-Q16/magick.exe mogrify -resize 32x $SCRIPT_PATH/src/assets/photos/**/*.placeholder.jpeg &>/dev/null
-/c/Program\ Files/ImageMagick-7.0.11-Q16/magick.exe mogrify -resize 32x $SCRIPT_PATH/src/assets/photos/**/*.placeholder.png &>/dev/null
-/c/Program\ Files/ImageMagick-7.0.11-Q16/magick.exe mogrify -resize 32x $SCRIPT_PATH/src/assets/photos/**/*.placeholder.jpg &>/dev/null
+magick.exe mogrify -resize 32x $SCRIPT_PATH/src/assets/photos/**/*.placeholder.jpeg #&>/dev/null
+magick.exe mogrify -resize 32x $SCRIPT_PATH/src/assets/photos/**/*.placeholder.png #&>/dev/null
+magick.exe mogrify -resize 32x $SCRIPT_PATH/src/assets/photos/**/*.placeholder.jpg #&>/dev/null
 
-python $SCRIPT_PATH/tools/setup.py
+# python $SCRIPT_PATH/tools/setup.py
