fbfcea831169095f81a360bb032550caf05ab08d
Roman Cherednychenko <Roman.Cheredichenko@gmail.com>
2021-04-20 19:27:41 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
  #!/usr/bin/env bash
- ng build --prod --base-href "https://www.agnesadventure.de/"
+ #ng build --prod --base-href "https://www.agnesadventure.de/"
  #npx angular-cli-ghpages --dir=dist/fotostrecke
  ng deploy --cname=www.agnesadventure.de
  
