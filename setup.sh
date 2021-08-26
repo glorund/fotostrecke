5cc42a553d6be193f3474f1944884d285727cb5e
Roman Cherednychenko <Roman.Cheredichenko@gmail.com>
2021-08-26 22:13:56 +0200

    resizing and watermarkes.

--- setup.sh
+++ setup.sh
@@ -4,16 +4,24 @@ SCRIPT_PATH=$(dirname "$0")
 
 export PATH=$PATH:/d/Program\ Files/ImageMagick-7.0.11-Q16-HDRI
 
-# mogrify is available
 cp -r src/sources/* src/assets/photos/ 
 # low res version of image
 python $SCRIPT_PATH/tools/duplicate.py min
+echo processing min
 magick.exe mogrify -resize 640x $SCRIPT_PATH/src/assets/photos/**/*.min.jpg # &>/dev/null
 
 # placeholder image for lazy loading
 python $SCRIPT_PATH/tools/duplicate.py placeholder
+echo processing placeholders
 magick.exe mogrify -resize 32x $SCRIPT_PATH/src/assets/photos/**/*.placeholder.jpg #&>/dev/null
 
 # TODO add watermark
-#/d/Program\ Files/ImageMagick-7.0.11-Q16-HDRI/composite.exe  -gravity Center ../../../sources/watermark.png source.jpeg output.jpeg
+for i in $SCRIPT_PATH/src/assets/photos/**/*.jpg
+do
+    if ! [[ "$i" =~ ".min.jpg" || "$i" =~ ".placeholder.jpg" ]]
+    then
+        composite -gravity Center src/sources/watermark.png  "$i" "$i"
+    fi
+done
+
 python $SCRIPT_PATH/tools/setup.py
